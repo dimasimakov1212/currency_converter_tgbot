aef82479125d3bca8bf1437575598dc4344239a6
DimaSimakov <dimarodeo@gmail.com>
2024-02-13 22:48:00 +0700

    Created function for checking date of currencies course

--- services.py
+++ services.py
@@ -1,7 +1,9 @@
 import json
 import os
 
+import pytz
 import requests
+import datetime
 
 
 def get_currencies_course():
@@ -18,11 +20,12 @@ def get_currencies_course():
         req.close()  # закрываем сеанс запроса
 
         data_out = json.loads(data_in)  # преобразуем полученные данные из формата json
-
         writing_json(data_out)  # записываем полученные данные в файл
 
+        return True
+
     if req.status_code != 200:
-        print("В настоящий момент сервис недоступен. Попробуйте позже.")
+        return False
 
 
 def writing_json(currencies_data):
@@ -35,4 +38,55 @@ def writing_json(currencies_data):
         json.dump(currencies_data, file, sort_keys=False, indent=4, ensure_ascii=False)
 
 
-# get_currencies_course()
+def reading_json():
+    """ Считывает данные из формата json """
+
+    # путь к файлу, в котором хранятся курсы валют
+    file_data = os.path.abspath(f'./currencies_course.json')
+
+    try:
+        with open(file_data, 'r', encoding='utf-8') as file:
+            data_list = json.load(file)
+        return data_list
+    except FileNotFoundError:
+        print('Файла пока не существует, будет создан новый файл')
+        data_list = []
+        return data_list
+
+
+def check_currencies_course_date():
+    """ Проверяет актуальность данных в сохраненном файле """
+
+    currencies_dict = reading_json()  # получаем список курсов валют из файла
+    currencies_date = currencies_dict['Date']  # получаем дату курса валют в файле
+
+    # определяем разницу в днях между текущей датой и датой курса валют
+    days_difference = get_days_difference(currencies_date)
+
+    # если сохраненный курс валют был получен день или более назад
+    if days_difference >= 1:
+        fresh_currencies_course = get_currencies_course()  # получаем свежий курс валют
+
+        return fresh_currencies_course
+
+    else:
+        return True
+
+
+def get_days_difference(date_time):
+    """ Считает разницу между текущей датой и полученной датой в днях """
+
+    desired_timezone = pytz.timezone('Europe/Moscow')  # устанавливаем часовой пояс
+    date_time_now = datetime.datetime.now()  # получаем текущие дату и время
+
+    time_now = date_time_now.astimezone(desired_timezone)  # текущее время с учетом часового пояса
+    # преобразуем время в формате ISO полученное из файла с учетом часового пояса
+    time_received = datetime.datetime.fromisoformat(date_time).astimezone(desired_timezone)
+
+    # считаем разницу между текущей датой и полученной датой в днях
+    days_difference = (time_now.date() - time_received.date()).days
+
+    return days_difference
+
+
+# check_currencies_course_date()
--- telegram_bot.py
+++ telegram_bot.py
@@ -4,6 +4,7 @@ import telebot
 
 from dotenv import load_dotenv
 
+from services import check_currencies_course_date
 
 load_dotenv('.env')  # загружаем данные из виртуального окружения
 
@@ -21,7 +22,20 @@ def start_bot(message):
 
         bot.send_message(message.chat.id, 'Привет, {0.first_name}!'.format(message.from_user))
         bot.send_message(message.chat.id, 'Бот позволяет производить конвертацию валют\n'
-                                          'USD, EUR, RUB по текущему курсу')
+                                          'USD, EUR, RUB по текущему курсу\n'
+                                          'Для получения дополнительной информации выберите команду\n'
+                                          '/help')
+
+        # проверяем актуальность сохраненных данных о курсе валют
+        # если данные устарели, то они будут обновлены
+        checking = check_currencies_course_date()
+
+        if checking:
+            bot.send_message(message.chat.id, 'Данные курсов валют обновлены')
+
+        else:
+            bot.send_message(message.chat.id, 'В настоящий момент невозможно получить'
+                                              ' свежий курс валют')
 
     # отправляем в бот список возможных команд
     if message.text == '/help':
